9d5969a503d1a1acafc191091fc441cdfbd91e4b
Manimaran Moorthi <manimaran.moorthi@expleogroup.com>
2023-04-06 10:05:55 +0530

    UI bit enhanced

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,10 +7,14 @@ const Header = () => {
                     <div className='header-menu-links hamburgerLink'>
                         <div className='subtitle-text'>Company</div>
                     </div>
-                    <div className="header-menu-links pe-2">
-                        <div className="header-menu-link d-flex align-items-center">
-                            <div className='p-text pl-2 text-uppercase'>Sign In</div>
-                            <div className='p-text pl-2 text-uppercase ms-4'>Sign Up</div>
+                    <div className="pe-2">
+                        <div className="d-flex align-items-center">
+                            <div className='p-text pl-2 text-uppercase'>
+                                <button type="button" className="btn btn-secondary">Sign In</button>
+                            </div>
+                            <div className='p-text pl-2 text-uppercase ms-4'>
+                                <button type="button" className="btn btn-primary">Sign Up</button>
+                            </div>
                         </div>
                     </div>
                 </div>
