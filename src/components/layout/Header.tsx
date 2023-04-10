ddbff8f084999fc698a39c691b072456492b3765
Manimaran Moorthi <manimaran.moorthi@expleogroup.com>
2023-04-10 09:32:51 +0530

    Search implemented with mobile responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,30 +1,80 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import { MenuIcon, SearchIcon } from '../reusableComponents/svgIcons';
+import { InputGroup } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 const Header = () => {
-    return (
-        <div className="main-header">
-            <div className='placeHolderPlaceOfHeader'></div>
-            <nav className="navbar navbar-default">
-                <div className="container-fluid flex-nowrap align-items-center">
-                    <div className='header-menu-links hamburgerLink'>
-                        {/* <div className="subtitle-text">Employees referral portal</div> */}
-                        <div className="subtitle-text">ER Portal</div>
+  return (
+    <div className="main-header">
+      <div className='placeHolderPlaceOfHeader'></div>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid flex-nowrap align-items-center">
+          <div className='header-menu-links hamburgerLink'>
+            {/* <div className="subtitle-text">Employees referral portal</div> */}
+            <div className="subtitle-text">ER Portal</div>
+          </div>
+          <div className="pe-2">
+            <div className='d-flex alin-items-center'>
+              <div>
+                <div className="d-flex align-items-center">
+                  <div>
+                    <div className="headerSearch mobHideHS">
+                      <InputGroup>
+                        <Form.Control
+                          type='search'
+                          placeholder="Search"
+                        />
+                        <Button variant="outline-secondary" id="button-addon2">
+                          <SearchIcon />
+                        </Button>
+                      </InputGroup>
                     </div>
-                    {/* <div>
-                        <div className="headerSearch"><input type="search" /></div>
-                    </div> */}
-                    <div className="pe-2">
-                        <div className="d-flex align-items-center">
-                            <div className='p-text pl-2 text-uppercase'>
-                                <button type="button" className="btn btn-secondary">Sign In</button>
-                            </div>
-                            <div className='p-text pl-2 text-uppercase ms-4'>
-                                <button type="button" className="btn btn-primary">Sign Up</button>
-                            </div>
-                        </div>
-                    </div>
+                  </div>
+                  <div className='mobileTopMenu ps-3'>
+                    <Dropdown>
+                      <Dropdown.Toggle id="dropdown-basic">
+                        <MenuIcon />
+                      </Dropdown.Toggle>
+
+                      <Dropdown.Menu>
+                        <Dropdown.ItemText><button type="button" className="btn btn-secondary w-100">Sign In</button></Dropdown.ItemText>
+                        <Dropdown.ItemText><button type="button" className="btn btn-primary w-100">Sign Up</button></Dropdown.ItemText>
+                        <Dropdown.Divider />
+                        <Dropdown.ItemText>
+                          <div className="headerSearch">
+                            <InputGroup>
+                              <Form.Control
+                                type='search'
+                                placeholder="Search"
+                              />
+                              <Button variant="outline-secondary" id="button-addon2">
+                                <SearchIcon />
+                              </Button>
+                            </InputGroup>
+                          </div>
+                        </Dropdown.ItemText>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </div>
                 </div>
-            </nav>
+              </div>
+              <div>
+                <div className="d-flex align-items-center ms-4 desktopTopButtons">
+                  <div className='p-text pl-2 text-uppercase'>
+                    <button type="button" className="btn btn-secondary">Sign In</button>
+                  </div>
+                  <div className='p-text pl-2 text-uppercase ms-4'>
+                    <button type="button" className="btn btn-primary">Sign Up</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  )
 }
 
 export default Header
--- src/components/reusableComponents/svgIcons.tsx
+++ src/components/reusableComponents/svgIcons.tsx
@@ -1,3 +1,11 @@
 export const HamburgerIcon = () => (
   <svg height="512" viewBox="0 0 16 16" width="512" xmlns="http://www.w3.org/2000/svg"><g id="_31" data-name="31"><path d="m15.5 4h-15a.5.5 0 0 1 0-1h15a.5.5 0 0 1 0 1z" /><path d="m15.5 9h-15a.5.5 0 0 1 0-1h15a.5.5 0 0 1 0 1z" /><path d="m15.5 14h-15a.5.5 0 0 1 0-1h15a.5.5 0 0 1 0 1z" /></g></svg>
 );
+
+export const MenuIcon = () => (
+  <svg height="512" viewBox="0 0 100 100" width="512" xmlns="http://www.w3.org/2000/svg"><g id="Layer_13" data-name="Layer 13"><circle cx="50" cy="50" r="8"/><circle cx="50" cy="75.5" r="8"/><circle cx="50" cy="24.5" r="8"/></g></svg>
+);
+
+export const SearchIcon = () => (
+  <svg id="Layer_1" enableBackground="new 0 0 128 128" height="512" viewBox="0 0 128 128" width="512" xmlns="http://www.w3.org/2000/svg"><path id="Search" d="m118.828 113.172-29.036-29.037c6.366-7.633 10.208-17.441 10.208-28.135 0-24.262-19.738-44-44-44s-44 19.738-44 44 19.738 44 44 44c10.694 0 20.502-3.842 28.135-10.208l29.037 29.037c.781.781 1.805 1.172 2.828 1.172s2.047-.391 2.828-1.172c1.563-1.563 1.563-4.095 0-5.657zm-98.828-57.172c0-19.85 16.148-36 36-36s36 16.15 36 36-16.148 36-36 36-36-16.15-36-36z"/></svg>
+);
